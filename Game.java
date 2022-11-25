1b339d949b0a9629aef8d8477d8bddabe5298fcc
Luca180405 <daguannoluca16@gmail.com>
2022-11-25 17:56:31 +0100

    versuch accept cmd für outside task

--- Game.java
+++ Game.java
@@ -43,6 +43,15 @@ class Game
     public String lives = ANSI_RED + "❤️ ❤️ ❤️ ❤️ ❤️" + ANSI_RESET;
     public String power = ANSI_YELLOW + "⚡⚡⚡⚡⚡" + ANSI_RESET;
 
+    private Boolean taskOutside = false;
+    private Boolean taskMarktcenter = false;
+    private Boolean taskTheatre = false;
+    private Boolean taskPub = false;
+    private Boolean taskGym = false;
+    private Boolean taskPoliceoffice = false;
+
+    private int checkRoomDone = 1;
+
     Room outside, theatre, pub, gym, policeoffice, marktcenter;
         
     /**
@@ -54,6 +63,7 @@ class Game
         parser = new Parser();
 
         inventoryItems[0] = ANSI_BLUE + "Bottle of water" + ANSI_RESET;
+        checkTasks();
     }
 
     /**
@@ -99,6 +109,22 @@ class Game
 
     }
 
+    private void checkTasks() {
+        if(taskOutside = true) {
+            if(currentRoom == theatre) {checkRoomDone += 1;}
+            if(currentRoom == pub) {checkRoomDone += 1;}
+            if(currentRoom == gym) {checkRoomDone += 1;}
+            if(currentRoom == policeoffice) {checkRoomDone += 1;}
+            if(currentRoom == marktcenter) {checkRoomDone += 1;}
+        }
+
+        if(checkRoomDone == 6) {
+            coins += 20;
+            System.out.println("You've completed a task and got 20 coins!");
+            System.out.println("Congrats!🎉");
+        }
+    }
+
     /**
      *  Main play routine.  Loops until end of play.
      */
@@ -137,6 +163,8 @@ class Game
             System.out.print("south ");
         if(currentRoom.westExit != null)
             System.out.print("west ");
+        System.out.println("");
+        System.out.println("Your Task: " + currentRoom.getTask());
         System.out.println();
     }
 
@@ -234,6 +262,17 @@ class Game
         if(drink.equals("Cola") && coins >= 8) {coins-=8;}
     }
 
+    private void accept() {
+        
+        if(currentRoom == outside) {taskOutside = true;}
+        if(currentRoom == marktcenter) {taskMarktcenter = true;}
+        if(currentRoom == theatre) {taskTheatre = true;}
+        if(currentRoom == pub) {taskPub = true;}
+        if(currentRoom == gym) {taskGym = true;}
+        if(currentRoom == policeoffice) {taskPoliceoffice = true;}
+
+    }
+
     private static int getRandomNumberInRange(int min, int max) {
 
 		if (min >= max) {
@@ -264,6 +303,8 @@ class Game
             goRoom(command);
         else if (commandWord.equals("look"))
             look();
+        else if (commandWord.equals("accept"))
+            accept();
         else if (commandWord.equals("donate"))
             donate();
         else if (commandWord.equals("coins"))
@@ -301,7 +342,7 @@ class Game
         System.out.println("around at the university.");
         System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help look coins donate shop offer inventory buy stats drinks  ");
+        System.out.println("   go quit help look accept coins donate shop offer inventory buy stats drinks drink  ");
     }
 
     /** 
