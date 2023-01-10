81c35465f6010412b52f918244f0006fcf1d6fb5
Luca180405 <daguannoluca16@gmail.com>
2023-01-10 07:56:15 +0100

    .

--- Game.java
+++ Game.java
@@ -427,7 +427,7 @@ class Game
     private void printHelp() 
     {
         System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+        System.out.println("around at Uelzen.");
         System.out.println();
         System.out.println("Your command words are:");
         System.out.println("   go quit help look coins donate shop offer inventory buy drinks drink  ");
